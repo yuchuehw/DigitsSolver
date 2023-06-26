40d073da1a0cf370c468e46198a1ad3204fd48b6
yuchuehw <137755504+yuchuehw@users.noreply.github.com>
2023-06-26 03:15:54 -0400

    Update solveDaily.py

--- solveDaily.py
+++ solveDaily.py
@@ -4,99 +4,94 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from solver import DigitSolver
-from collections import defaultdict    
-    
+from collections import defaultdict
+
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-# chrome_options.add_argument("--headless")
 
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.get('https://www.nytimes.com/games/digits')
-WebDriverWait(driver, 10).until(
-  EC.element_to_be_clickable((By.ID, 'play-button'))
-).click()
+def click_element(element_id, error_message):
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, element_id))
+        ).click()
+    except:
+        input(f"I am stuck, {error_message}")
 
-WebDriverWait(driver, 10).until(
-  EC.element_to_be_clickable((By.ID, 'close-help'))
-).click()
+def play_button_click():
+    click_element('play-button', "play button doesn't work!")
 
-for question in range(5):
-    buttons = defaultdict(lambda:list())
-    solution = []
-    skip = []
-    
-    for x in range(5):
-        if x in skip:
+def close_help_button_click():
+    click_element('close-help', "close help button doesn't work!")
+
+def next_puzzle_button_click():
+    click_element('next-puzzle-button', "next puzzle button doesn't work!")
+
+def number_button_click(button_id, error_message):
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, button_id))
+        ).click()
+    except:
+        input(f"I am stuck, {error_message}")
+
+def combine_numbers(step_list, buttons):
+    i1, i2 = buttons[step_list[0]].pop(), buttons[step_list[2]].pop()
+
+    number_button_click(f'number-pos-{i1}', "I can't click the first number")
+    number_button_click(step_list[1], "I can't click the operator")
+    number_button_click(f'number-pos-{i2}', "I can't click the second number")
+
+    buttons[step_list[4]].append(i2)
+
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.text_to_be_present_in_element((By.ID, f'number-pos-{i2}'), step_list[4])
+        )
+    except:
+        input("I am stuck, the number doesn't seem to combine!")
+
+def main():
+    driver.get('https://www.nytimes.com/games/digits')  # Replace with the actual URL of the game
+
+    play_button_click()
+    close_help_button_click()
+
+    for question in range(5):
+        buttons = defaultdict(list)
+        solution = []
+
+        for x in range(6):
+            button_id = f'number-pos-{x}'
+            button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.ID, button_id))
+            )
+            buttons[button.get_attribute('innerHTML')].append(x)
+
+        target = driver.find_element(
+            By.ID,
+            'target'
+        ).get_attribute('innerHTML')
+
+        solver = DigitSolver([int(x) for x in buttons], int(target))
+        solver.printer_setter(lambda x: solution.append(x))
+        solver.solve(True)
+        print(list(buttons), target)
+
+        for step in solution[0]:
+            print(step)
+            step_list = step.split(' ')
+            combine_numbers(step_list, buttons)
+
+        if question == 4:
+            input("Press Enter to exit the program")
             continue
-        button = driver.find_element(By.ID, f'number-pos-{x}')
-        buttons[button.get_attribute('innerHTML')].append(x)  
 
+        next_puzzle_button_click()
 
-      
-    target = driver.find_element(
-                                  By.ID,
-                                  'target'
-                                ).get_attribute('innerHTML')
-    
-    solver = DigitSolver([int(x) for x in
-                          list(buttons)],int(target))
-    solver.printer_setter(lambda x:solution.append(x))
-    solver.solve(True)
-    
+    driver.quit()
 
-    for step in solution[0]:
-        
-        print(step)
-        step_list = step.split(' ')
-        # for component in step_list[:3]:
-        i1,i2 = buttons[step_list[0]].pop(), buttons[step_list[2]].pop()
-
-        print(i1,i2)
-        try:
-            WebDriverWait(driver, 10).until(
-              EC.text_to_be_present_in_element((By.ID, f'number-pos-{i1}'),step_list[0])
-            )
-        except:
-            input("i am stuck")
-
-        try:
-            WebDriverWait(driver, 10).until(
-              EC.element_to_be_clickable((By.ID, f'number-pos-{i1}'))
-            ).click()
-        except:
-            input("i am stuck")
-      
-
-        try:
-            WebDriverWait(driver, 10).until(
-              EC.element_to_be_clickable((By.ID, step_list[1]))
-            ).click()
-        except:
-            input("i am stuck")
-          
-        try:
-            WebDriverWait(driver, 10).until(
-              EC.text_to_be_present_in_element((By.ID, f'number-pos-{i2}'),step_list[2])
-            )
-        except:
-            input("i am stuck")
-          
-        try:
-            WebDriverWait(driver, 10).until(
-              EC.element_to_be_clickable((By.ID, f'number-pos-{i2}'))
-            ).click()
-        except:
-            input("i am stuck")
-          
-        
-        buttons[step_list[4]].append(i2)
-
-    if question == 4:
-        continue
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.ID, "next-puzzle-button"))
-    ).click()
-
-input("press enter to close session")
+if __name__ == "__main__":
+    main()
