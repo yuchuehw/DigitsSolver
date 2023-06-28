a99b112b74adce5be2e35631063624543e69386c
yuchuehw <137755504+yuchuehw@users.noreply.github.com>
2023-06-28 17:40:38 -0400

    Update test.py

--- test.py
+++ test.py
@@ -5,13 +5,13 @@ def test_solve_single_solution():
     # Assert that at least one solution is found
     assert(solution_found > 0)
 
-def test_solve_multiple_solutions(self):
+def test_solve_multiple_solutions():
     solver = DigitSolver([2, 4, 6, 8], 12)
     solution_found = solver.solve()
     # Assert that multiple solutions are found
     assert(solution_found > 1)
 
-def test_solve_no_solution(self):
+def test_solve_no_solution():
     solver = DigitSolver([1, 2, 3, 4], 100)
     solution_found = solver.solve()
     # Assert that no solution is found
