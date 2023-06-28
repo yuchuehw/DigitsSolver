24194a39d897d0b4f50803b3590bc31e6e4e24e5
yuchuehw <137755504+yuchuehw@users.noreply.github.com>
2023-06-28 01:40:17 -0400

    Update solveMany.py

--- solver/util/solveMany/solveMany.py
+++ solver/util/solveMany/solveMany.py
@@ -2,19 +2,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 def read_problem(driver, buttons):
     for x in range(6):
-        button_id = f'number-pos-{x}'
+        button_id = f"number-pos-{x}"
         button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, button_id))
         )
-        buttons[button.get_attribute('innerHTML')].append(x)
+        buttons[button.get_attribute("innerHTML")].append(x)
 
-    target = driver.find_element(
-        By.ID,
-        'target'
-    ).get_attribute('innerHTML')
+    target = driver.find_element(By.ID, "target").get_attribute("innerHTML")
     return target
+
 
 def click_element(driver, element_id, error_message):
     try:
@@ -24,14 +23,18 @@ def click_element(driver, element_id, error_message):
     except:
         input(f"I am stuck, {error_message}")
 
+
 def play_button_click(driver):
-    click_element(driver, 'play-button', "play button doesn't work!")
+    click_element(driver, "play-button", "play button doesn't work!")
+
 
 def close_help_button_click(driver):
-    click_element(driver, 'close-help', "close help button doesn't work!")
+    click_element(driver, "close-help", "close help button doesn't work!")
+
 
 def next_puzzle_button_click(driver):
-    click_element(driver, 'next-puzzle-button', "next puzzle button doesn't work!")
+    click_element(driver, "next-puzzle-button", "next puzzle button doesn't work!")
+
 
 def number_button_click(driver, button_id, error_message):
     try:
@@ -40,6 +43,7 @@ def number_button_click(driver, button_id, error_message):
         ).click()
     except:
         input(f"I am stuck, {error_message}")
+
 
 def wait_til_combine(driver, button_id, expect_value):
     try:
@@ -53,13 +57,14 @@ def wait_til_combine(driver, button_id, expect_value):
 def combine_numbers(driver, step_list, buttons):
     i1, i2 = buttons[step_list[0]].pop(), buttons[step_list[2]].pop()
 
-    number_button_click(driver, f'number-pos-{i1}', "I can't click the first number")
+    number_button_click(driver, f"number-pos-{i1}", "I can't click the first number")
     number_button_click(driver, step_list[1], "I can't click the operator")
-    number_button_click(driver, f'number-pos-{i2}', "I can't click the second number")
+    number_button_click(driver, f"number-pos-{i2}", "I can't click the second number")
 
     buttons[step_list[4]].append(i2)
 
-    wait_til_combine(driver, f'number-pos-{i2}', step_list[4])
+    wait_til_combine(driver, f"number-pos-{i2}", step_list[4])
+
 
 def back_to_puzzles_button_click(driver):
-    click_element('back-to-puzzles-button', "back to puzzles button doesn't work!")
+    click_element("back-to-puzzles-button", "back to puzzles button doesn't work!")
