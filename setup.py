1e6affad8ac57aff3793d76b479cdee4789407ea
yuchuehw <137755504+yuchuehw@users.noreply.github.com>
2023-06-29 16:20:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ from setuptools import setup, find_packages
 setup(
     name="DigitsSolver",
     version="1.0",
+    author="yuchuehw",
+    author_email="yuchuehw@outlook.com",
+    description="Digits Solver is a Python program that solves a digits puzzle game by finding mathematical operations that can be applied to a set of starting digits to obtain a target digit.",
     packages=find_packages(),
 )
