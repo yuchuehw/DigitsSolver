260ab9664cda1315def5c9522aea2aebf6d50633
yuchuehw <137755504+yuchuehw@users.noreply.github.com>
2023-06-30 14:52:24 -0400

    Update __main__.py

--- solver/__main__.py
+++ solver/__main__.py
@@ -1,9 +1,66 @@
-from solver import DigitSolver
+"""
+This module provides a command-line interface for solving the New York Times' 
+Digits game using the DigitSolver class.
+
+Usage:
+    python solver [starting_digits] [target_digit] [-os]
+
+Arguments:
+    starting_digits (int): Numbers allowed in the game.
+    Must be provided as separate integer values.
+    target_digit (int): Target number that needs to be achieved.
+    -os, --onesolution: Optional flag to find only one solution.
+    By default, all solutions are found.
+
+Example:
+    python main.py 3 12 15 20 23 25 439
+
+    This command runs the DigitSolver with starting digits [3, 12, 15, 20, 23, 25]
+    and target digit 439,
+    finding all solutions to the game.
+
+Console Mode:
+    If no command-line arguments are provided,
+    a demo of using the DigitSolver class as an import will be displayed.
+
+    Here's the full code:
+
+    ```
+    from solver import DigitSolver
+
+    solver = DigitSolver([3, 12, 15, 20, 23, 25], 439)
+    solution_count = solver.solve(False)
+    print(f"we found {solution_count} solution(s)")
+    ```
+
+    The result will be printed, showing the number of solutions found.
+"""
+
 import argparse
 import sys
+from solver import DigitSolver
 
 
 def main():
+    """
+    Main function for running the Digits game solver.
+
+    This function parses the command-line arguments,
+    creates an instance of the DigitSolver class,
+    and prints the number of solutions found for the game.
+
+    Command-line Arguments:
+        - starting_digits (int): Numbers allowed in the game.
+        - target_digit (int): Target number that needs to be achieved.
+        - -os, --onesolution: Optional flag to find only one solution.
+        By default, all solutions are found.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
     parser = argparse.ArgumentParser(description="New York Times' Digits solver")
     parser.add_argument(
         "starting_digits",
@@ -48,7 +105,7 @@ here's the result:
     solution_count = solver.solve(one_solution)
     print(f"we found {solution_count} solution(s)")
     if not sys.argv[1:]:
-        print("we also support console mode use [-h] to learn more.")
+        print("we also support CLI mode use [-h] to learn more.")
 
 
 if __name__ == "__main__":
