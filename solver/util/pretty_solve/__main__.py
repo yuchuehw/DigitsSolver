9c598a7980a0c8c3e96b1bc3035073e61ab37519
yuchuehw <137755504+yuchuehw@users.noreply.github.com>
2023-07-02 01:54:22 -0400

    Update __main__.py

--- solver/util/pretty_solve/__main__.py
+++ solver/util/pretty_solve/__main__.py
@@ -1,3 +1,26 @@
+"""
+Digits Game Solver
+
+This module provides a command-line interface to solve the Digits game. The Digits game is a mathematical
+puzzle where the goal is to reach a target number by performing arithmetic operations on a list of given numbers.
+The module uses the concurrent.futures module to execute the solver in a separate thread, allowing for real-time
+interaction with the solutions.
+
+Usage:
+    1. Run the script from the command line.
+    2. Enter the numbers allowed, separated by spaces.
+    3. Enter the target number.
+    4. Use the Enter key to loop through possible solutions.
+    5. Press Ctrl+C to exit the program.
+
+Note: The module requires the 'solver' package to be installed, which contains the 'solver' module with the
+implementation of the DigitSolver class.
+
+Author: Yu-Chueh Wang
+Version: 1.7.1
+
+"""
+
 import concurrent.futures
 import os
 import sys
@@ -16,14 +39,42 @@ from solver.solver import DigitSolver
 Executor = concurrent.futures.ThreadPoolExecutor
 
 
-def main():
+def main() -> None:
+    """
+    Main function to run the Digits game solver.
+
+    This function prompts the user for inputs, initializes the DigitSolver,
+    and starts the solver process. It allows the user to loop through possible solutions
+    by pressing the enter key and exits the program on Ctrl+C.
+
+    Returns:
+        None
+    """
     os.system("clear")
     holding_list = []
 
     def mf(step: list) -> None:
+        """
+        Custom printer function to store steps in the holding list.
+
+        Args:
+            step (list): A list of steps to solve the Digits game.
+
+        Returns:
+            None
+        """
         holding_list.append(step)
 
-    def alternative_input(prompt: str = ""):
+    def alternative_input(prompt: str = "") -> str:
+        """
+        Function to handle user input with KeyboardInterrupt support.
+
+        Args:
+            prompt (str, optional): The input prompt. Defaults to "".
+
+        Returns:
+            str: The user's input string.
+        """
         try:
             input_ = input(prompt)
         except KeyboardInterrupt:
@@ -33,6 +84,15 @@ def main():
         return input_
 
     def get_integer_list_input(prompt: str) -> list:
+        """
+        Function to get a list of integers from user input.
+
+        Args:
+            prompt (str): The input prompt.
+
+        Returns:
+            list: A list of integers entered by the user.
+        """
         while True:
             input_str = alternative_input(prompt)
             number_list = input_str.split()
@@ -49,6 +109,15 @@ def main():
                 return valid_numbers
 
     def get_integer_input(prompt: str) -> int:
+        """
+        Function to get an integer from user input.
+
+        Args:
+            prompt (str): The input prompt.
+
+        Returns:
+            int: The integer entered by the user.
+        """
         while True:
             input_str = alternative_input(prompt)
             try:
@@ -69,7 +138,7 @@ def main():
     os.system("clear")
 
     solver = DigitSolver(number_list, target)
-    solver.printer_setter(mf)
+    solver.printer = mf
 
     with Executor() as executor:
         future = executor.submit(solver.solve)
